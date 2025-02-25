862bc7a42f234fda8f037bef8fa5779327b9a5fc
Claudss90 <claudio-s.s@hotmail.com>
2025-02-25 17:48:10 -0300

    Initial commit

--- src/com/claudio/javacurso/LeituraDadosTeclado.java
+++ src/com/claudio/javacurso/LeituraDadosTeclado.java
@@ -7,21 +7,6 @@ public class LeituraDadosTeclado {
 
         Scanner scan = new Scanner((System.in));
 
-       // System.out.println("Digite seu nome completo:");
-        //String nomeCompleto = scan.nextLine();
-        //System.out.println("Seu nome completo é:" + nomeCompleto);
-
-        //System.out.println("Digite seu primeiro nome:");
-        //String primeiroNome = scan.next();
-        //System.out.println("Seu primeiro nome é:" + primeiroNome);
-
-//        System.out.println("Digite sua idade:");
-//        int idade = scan.nextInt();
-//        System.out.println("Sua idade é:" + idade);
-//
-//        System.out.println("Digite sua altura:");
-//        double altura = scan.nextDouble();
-//        System.out.println("Sua altura é:" + altura);
         System.out.println("Digite o seu primeiro nome, idade, quantidade de filhos, altura e se tem pet. ");
         String primeiroNome = scan.next();
         int idade = scan.nextInt();
@@ -30,10 +15,10 @@ public class LeituraDadosTeclado {
         boolean temPet = scan.nextBoolean();
 
         System.out.println("Você digitou os  seguintes valores: ");
-        System.out.println("Primeiro nome: " +primeiroNome);
+        System.out.println("Primeiro nome : " +primeiroNome);
         System.out.println("Idade: " +idade);
         System.out.println("Quantidade de filhos: " +qtdFilhos);
-        System.out.println("Altura: " +altura);
+        System.out.println("Altura : " +altura);
         System.out.println("Tem pet? " +temPet);
 
     }
